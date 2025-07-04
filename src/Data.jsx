92822f60624d3a49c8a02b4fbea32a1841054e12
Raidul <raidul2105@gmail.com>
2025-07-04 22:04:54 +0700

    Fix image import path: project3.JPG

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,6 +1,6 @@
 import Project1 from './assets/project1.jpg';
 import Project2 from './assets/project2.jpg';
-import Project3 from './assets/project3.jpg';
+import Project3 from './assets/project3.JPG';
 import Project4 from './assets/project4.jpg';
 import Project5 from './assets/project5.jpg';
 import Project6 from './assets/project6.jpg';
